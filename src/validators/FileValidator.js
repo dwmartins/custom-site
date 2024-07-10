13994c236946b1e4ad32b2c8b95a1c84300d35a0
Douglas Martins <douglas5422@outlook.com>
2024-07-09 21:47:34 -0300

    feat: Iniciando upload de imagens do site

---
+++ src/validators/FileValidator.js
@@ -0,0 +1,39 @@
+import { alertStore } from "@/store/alertStore";
+
+class FileValidator {
+    validExtensions = ['image/jpeg', 'image/jpg', 'image/png'];
+    fileSize = 5 * 1024 * 1024; // 5MB
+
+    validIconExtensions = ['image/vnd.microsoft.icon', 'image/x-icon'];
+    iconFileSize = 5 * 1024 * 1024; // 5MB
+
+    validImage(image) {
+        if (image.size > this.fileSize) {
+            alertStore.addAlert('info', 'A imagem deve ter no máximo 5MB.');
+            return false;
+        }
+
+        if (!this.validExtensions.includes(image.type)) {
+            alertStore.addAlert('info', 'O formato da imagem deve ser (png, jpg ou jpeg)');
+            return false;
+        }
+
+        return true;
+    }
+
+    validIcon(icon) {
+        if (icon.size > this.iconFileSize) {
+            alertStore.addAlert('info', 'O ícone deve ter no máximo 5MB.');
+            return false;
+        }
+    
+        if (!this.validIconExtensions.includes(icon.type)) {
+            alertStore.addAlert('info', 'O formato do ico deve ser um ícone ICO ou JPG, JPEG ou PNG.');
+            return false;
+        }
+    
+        return true;
+    }
+}
+
+export default new FileValidator();
--- src/views/admin/BasicInformationView.vue
+++ src/views/admin/BasicInformationView.vue
@@ -1,15 +1,100 @@
 <template>
     <section id="basicInformationView">
-        <h1>Informações Básicas do site</h1>
+        <h5 class="custom_dark mb-3">Informações básicas</h5>
+
+        <form class="container-fluid shadow rounded-2 mb-5">
+            <div class="row p-3">
+                <p class="custom_dark fs-7" >Informações básicas do site</p>
+                <hr class="custom-hr-dark">
+
+                <div class="container mt-3 mb-4">
+                <div class="row">
+                    <!-- Logo image -->
+                    <div class="col-sm-12 col-lg-4 logo-image mb-4">
+                        <img :src="previewLogoImg ? previewLogoImg : defaultLogoImage" alt="Logotipo">
+                    </div>
+                    <div class="col-sm-12 col-lg-8">
+                        <p class="custom_dark">Escolha a imagem do seu logotipo</p>
+                        <p class="custom_dark opacity-75 fs-7">Recomenda-se dimensão de 250px por 125px, tamanho máximo 5 MB, (JPG, JPEG ou PNG.)</p>
+
+                        <div v-if="previewLogoImg" class="d-flex gap-2 align-items-center">
+                            <button @click="cancelSetLogoImg()" type="button" class="btn btn-sm btn-danger">Cancelar</button>
+                            <span>{{ logoImage.name }}</span>
+                        </div>
+                    
+                        <input v-else @change="setLogoImage($event)" class="form-control custom_focus form-control-sm" type="file" :accept="acceptImg">
+                        
+                    </div>
+                </div>
+            </div>
+            </div>
+        </form>
     </section>
 </template>
 
 <script>
+import { siteInfoStore } from '@/store/siteInfoStore';
+import defaultLogo from '@/assets/img/default/defaultLogo.png';
+import FileValidator from '@/validators/FileValidator';
+
 export default {
-    name: 'BasicInformationView'
+    name: 'BasicInformationView',
+
+    data() {
+        return {
+            acceptImg: "image/jpeg, image/jpg, image/png",
+            defaultLogoImage: siteInfoStore.constants.logoImage ? `${this.$API_URL}/uploads/systemImages/${siteInfoStore.constants.logoImage}` : defaultLogo,
+            previewLogoImg: "",
+            logoImage: null
+        }
+    },
+
+    methods: {
+        setLogoImage(event) {
+            const fileInput = event.target;
+            const file = fileInput.files?.[0];
+            
+            if(file) {
+                if(FileValidator.validImage(file)) {
+                    this.logoImage = file;
+                    const reader = new FileReader();
+
+                    reader.onload = () => {
+                        this.previewLogoImg = reader.result?.toString();
+                    };
+                    reader.readAsDataURL(file);
+                } else {
+                    fileInput.value = '';
+                }
+            }
+        },
+
+        cancelSetLogoImg() {
+            this.previewLogoImg = "";
+        }
+    }
 };
 </script>
 
 <style scoped>
+.logo-image img{
+    width: 200px;
+    height: 100px;
+    object-fit: cover;
+}
 
+.favicon img {
+    width: 60px;
+    height: 60px;
+}
+
+.default-image img {
+    width: 150px;
+    height: 150px;
+}
+
+#workSchedule::placeholder {
+    color: #b1b1b1 !important;
+    font-size: 14px;
+}
 </style>
